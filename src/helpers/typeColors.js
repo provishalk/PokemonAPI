b5c6509aac94530e744720d58b4cddfb8ecaf8e4
vishal sharma <provishalk@gmail.com>
2021-05-16 10:42:54 +0530

    first commit

---
+++ src/helpers/typeColors.js
@@ -0,0 +1,22 @@
+const typeColors = {
+    bug: '#729f3f',
+    dragon: '#53a4cf',
+    fairy: '#fdb9e9',
+    fire: '#FFA500',
+    ghost: '#7b62a3',
+    ground: '#f7de3f',
+    normal: '#a4acaf',
+    pyschic: '#f366b9',
+    steel: '#9eb7b',
+    dark: '#707070',
+    electric: '#eed535',
+    fighting: '#d56723',
+    flying: '#3dc7ef',
+    grass: '#008000',
+    ice: '#51c4e7',
+    poison: 'rgb(255,0,50)',
+    rock: '#a38c21',
+    water: '#4592c4'
+}
+
+export default typeColors;
